d4e168b2197b702b98c77458175f879d6f292d60
zhongpei <1000@intry.cn>
2022-12-02 16:50:53 +0800

    0

--- src/IczpNet.Chat.HttpApi/Controllers/FriendshipController.cs
+++ src/IczpNet.Chat.HttpApi/Controllers/FriendshipController.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using Volo.Abp;
 
 namespace IczpNet.Chat.Controllers
 {
+
+    [Area(ChatRemoteServiceConsts.ModuleName)]
+    [RemoteService(Name = ChatRemoteServiceConsts.RemoteServiceName)]
+    [Route($"Api/{ChatRemoteServiceConsts.ModuleName}[Controller]/[Action]")]
     public class FriendshipController : ChatController
     {
         protected IFriendshipAppService FriendshipAppService { get; }
