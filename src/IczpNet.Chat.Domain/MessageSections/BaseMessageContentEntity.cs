f1296bef0d0df32ddcc425121398f51e46af5db9
zhongpei <1000@intry.cn>
2022-12-06 15:32:36 +0800

    MessageContent_AddProp_OwnerId

--- src/IczpNet.Chat.Domain/MessageSections/BaseMessageContentEntity.cs
+++ src/IczpNet.Chat.Domain/MessageSections/BaseMessageContentEntity.cs
@@ -1,15 +1,21 @@
 ﻿using IczpNet.AbpCommons.DataFilters;
 using IczpNet.Chat.BaseEntitys;
+using IczpNet.Chat.ChatObjects;
+using IczpNet.Chat.DataFilters;
 using IczpNet.Chat.MessageSections.Messages;
 using System;
 using System.Collections.Generic;
 
 namespace IczpNet.Chat.MessageSections
 {
-    public abstract class BaseMessageContentEntity : BaseEntity<Guid>, IMessageContent, IIsActive
+    public abstract class BaseMessageContentEntity : BaseEntity<Guid>, IMessageContent, IIsActive, IChatOwner<Guid?>
     {
         public virtual bool IsActive { get; set; }
 
         public virtual IList<Message> MessageList { get; set; }
+
+        public virtual Guid? OwnerId { get; set; }
+
+        public virtual ChatObject Owner { get; set; }
     }
 }
