998536deac463bef291b65cd1fd1332d06436763
zhongpei <1000@intry.cn>
2023-02-27 17:51:26 +0800

    ChatObjectDataSeedContributor

---
+++ src/IczpNet.Chat.Domain/ChatObjects/ChatObjectDataSeedContributor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.Uow;
+
+namespace IczpNet.Chat.ChatObjects
+{
+    public class ChatObjectDataSeedContributor : IDataSeedContributor, ITransientDependency
+    {
+        protected ILogger<ChatObjectDataSeedContributor> Logger { get; }
+        protected IConfiguration Configuration { get; }
+        protected ICurrentTenant CurrentTenant { get; }
+        protected IRepository<ChatObject, Guid> Repository { get; }
+
+        public ChatObjectDataSeedContributor(
+            IConfiguration configuration,
+            ICurrentTenant currentTenant,
+            IRepository<ChatObject, Guid> repository,
+            ILogger<ChatObjectDataSeedContributor> logger)
+        {
+            Configuration = configuration;
+            CurrentTenant = currentTenant;
+            Repository = repository;
+            Logger = logger;
+        }
+
+        [UnitOfWork]
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            using (CurrentTenant.Change(context?.TenantId))
+            {
+                await CreateAsync();
+            }
+        }
+
+        private async Task CreateAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
