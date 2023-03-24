ef062e0fe0e68c0951d89293b078b7df6e28df68
zhongpei <1000@intry.cn>
2023-03-24 17:24:00 +0800

    Abp7_1_0

--- host/IczpNet.Chat.AuthServer/Migrations/20230324092156_Abp7_1_0.Designer.cs
+++ host/IczpNet.Chat.AuthServer/Migrations/20230324092156_Abp7_1_0.Designer.cs
@@ -4,6 +4,7 @@ using IczpNet.Chat.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace IczpNet.Chat.Migrations
 {
     [DbContext(typeof(AuthServerDbContext))]
-    partial class AuthServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230324092156_Abp7_1_0")]
+    partial class Abp7_1_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ host/IczpNet.Chat.HttpApi.Host/Migrations/20230324092307_Abp7_1_0.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IczpNet.Chat.Migrations
+{
+    /// <inheritdoc />
+    public partial class Abp7_1_0 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
