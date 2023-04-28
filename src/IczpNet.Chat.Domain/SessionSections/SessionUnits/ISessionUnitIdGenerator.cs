3dc1cc6adb54c426d968c1f334f0f1327001f36e
zhongpei <1000@intry.cn>
2023-04-28 16:29:38 +0800

    Resolve

--- src/IczpNet.Chat.Domain/SessionSections/SessionUnits/ISessionUnitIdGenerator.cs
+++ src/IczpNet.Chat.Domain/SessionSections/SessionUnits/ISessionUnitIdGenerator.cs
@@ -5,5 +5,7 @@ namespace IczpNet.Chat.SessionSections.SessionUnits
     public interface ISessionUnitIdGenerator
     {
         Guid Create(long ownerId, long destinationId);
+
+        Guid Resolve(string sessionUnitId);
     }
 }
--- src/IczpNet.Chat.Domain/SessionSections/SessionUnits/SessionUnitIdGenerator.cs
+++ src/IczpNet.Chat.Domain/SessionSections/SessionUnits/SessionUnitIdGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using IczpNet.Chat.TextTemplates;
+using Pipelines.Sockets.Unofficial.Buffers;
 using System;
+using System.Linq;
 using Volo.Abp;
 using Volo.Abp.Domain.Services;
 
@@ -7,10 +9,16 @@ namespace IczpNet.Chat.SessionSections.SessionUnits
 {
     public class SessionUnitIdGenerator : DomainService, ISessionUnitIdGenerator
     {
-        public Guid Create(long ownerId, long destinationId)
+        public virtual Guid Create(long ownerId, long destinationId)
         {
             var id = string.Format("{0}_{1}", IntStringHelper.IntToString(ownerId), IntStringHelper.IntToString(destinationId));
             return GuidGenerator.Create();
+        }
+
+        public virtual long[] Resolve(string sessionUnitId)
+        {
+            var arr = sessionUnitId.Split('_').Select(x => IntStringHelper.StringToInt(x)).ToArray();
+            return arr; 
         }
     }
 }
