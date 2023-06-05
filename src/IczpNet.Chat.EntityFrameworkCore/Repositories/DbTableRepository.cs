80163660b63fb3c38514141ca5830b1d54b9d42e
zhongpei <1000@intry.cn>
2023-06-05 16:09:44 +0800

    0

--- src/IczpNet.Chat.EntityFrameworkCore/Repositories/DbTableRepository.cs
+++ src/IczpNet.Chat.EntityFrameworkCore/Repositories/DbTableRepository.cs
@@ -18,22 +18,20 @@ namespace IczpNet.Chat.Repositories
             var context = await GetDbContextAsync();
 
             var sql = @$"
-SELECT * From (
-    SELECT
-            QUOTENAME(SCHEMA_NAME(sOBJ.schema_id)) + '.' + QUOTENAME(sOBJ.name) AS [TableName]
-            , SUM(sPTN.Rows) AS [RowCount]
-    FROM 
-            sys.objects AS sOBJ
-            INNER JOIN sys.partitions AS sPTN
-                ON sOBJ.object_id = sPTN.object_id
-    WHERE
-            sOBJ.type = 'U'
-            AND sOBJ.is_ms_shipped = 0x0
-            AND index_id < 2 -- 0:Heap, 1:Clustered
-    GROUP BY 
-            sOBJ.schema_id
-            , sOBJ.name
-) DbTable
+SELECT
+        QUOTENAME(SCHEMA_NAME(sOBJ.schema_id)) + '.' + QUOTENAME(sOBJ.name) AS [TableName]
+        , SUM(sPTN.Rows) AS [RowCount]
+FROM 
+        sys.objects AS sOBJ
+        INNER JOIN sys.partitions AS sPTN
+            ON sOBJ.object_id = sPTN.object_id
+WHERE
+        sOBJ.type = 'U'
+        AND sOBJ.is_ms_shipped = 0x0
+        AND index_id < 2 -- 0:Heap, 1:Clustered
+GROUP BY 
+        sOBJ.schema_id
+        , sOBJ.name
 ";
             await Task.CompletedTask;
 
