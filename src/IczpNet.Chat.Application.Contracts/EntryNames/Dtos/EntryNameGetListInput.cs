f7c2eda4c095e8e9c3b165438d635fbc896c214f
zhongpei <1000@intry.cn>
2023-06-21 15:44:47 +0800

    0

--- src/IczpNet.Chat.Application.Contracts/EntryNames/Dtos/EntryNameGetListInput.cs
+++ src/IczpNet.Chat.Application.Contracts/EntryNames/Dtos/EntryNameGetListInput.cs
@@ -5,13 +5,28 @@ namespace IczpNet.Chat.EntryNames.Dtos;
 
 public class EntryNameGetListInput : BaseTreeGetListInput<Guid>
 {
+    /// <summary>
+    /// 是否选择
+    /// </summary>
     public virtual bool? IsChoice { get; set; }
 
+    /// <summary>
+    /// 是否唯一
+    /// </summary>
     public virtual bool? IsUniqued { get; set; }
 
+    /// <summary>
+    /// 是否必填
+    /// </summary>
     public virtual bool? IsRequired { get; set; }
 
+    /// <summary>
+    /// 是否固定
+    /// </summary>
     public virtual bool? IsStatic { get; set; }
 
+    /// <summary>
+    /// 是否公开
+    /// </summary>
     public virtual bool? IsPublic { get; set; }
 }
