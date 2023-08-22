9663a6df18d42f136aeb9868edb102b40274e4e7
zhongpei <1000@intry.cn>
2023-08-22 15:25:59 +0800

    mig:Abp7_3_0

--- host/IczpNet.Chat.AuthServer/Migrations/20230822072523_Abp7_3_0.Designer.cs
+++ host/IczpNet.Chat.AuthServer/Migrations/20230822072523_Abp7_3_0.Designer.cs
@@ -4,6 +4,7 @@ using IczpNet.Chat.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace IczpNet.Chat.Migrations
 {
     [DbContext(typeof(AuthServerDbContext))]
-    partial class AuthServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230822072523_Abp7_3_0")]
+    partial class Abp7_3_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
