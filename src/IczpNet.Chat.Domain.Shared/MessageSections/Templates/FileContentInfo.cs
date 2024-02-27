eff4eafca36a543bceeb499b81c2ab272298ae56
zhongpei <1000@intry.cn>
2024-02-27 18:02:23 +0800

    0

--- src/IczpNet.Chat.Domain.Shared/MessageSections/Templates/FileContentInfo.cs
+++ src/IczpNet.Chat.Domain.Shared/MessageSections/Templates/FileContentInfo.cs
@@ -13,25 +13,5 @@ public class FileContentInfo : MessageContentAttachmentsInfoBase, IContentInfo
     /// <summary>
     /// 文件地址
     /// </summary>
-    public virtual string Url { get; set; }
-
-    /// <summary>
-    /// 文件地址
-    /// </summary>
     public virtual string ActionUrl { get; set; }
-
-    /// <summary>
-    /// ContentType
-    /// </summary>
-    public virtual string ContentType { get; set; }
-
-    /// <summary>
-    /// 文件后缀名
-    /// </summary>
-    public virtual string Suffix { get; set; }
-
-    /// <summary>
-    /// 大小 ContentLength(Size)
-    /// </summary>
-    public virtual long? ContentLength { get; set; }
 }
