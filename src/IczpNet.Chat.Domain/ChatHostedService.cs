32f45a1acaf51e55dd1cc5cc900f4805720eac68
zhongpei <1000@intry.cn>
2025-03-06 17:09:36 +0800

    ChatHostedService

---
+++ src/IczpNet.Chat.Domain/ChatHostedService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Services;
+
+namespace IczpNet.Chat;
+
+public class ChatHostedService : DomainService, IHostedService, ISingletonDependency
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Logger.LogInformation($"App Start,HostName:{Dns.GetHostName()}");
+        await Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        Logger.LogInformation($"App Stop,HostName:{Dns.GetHostName()}");
+        await Task.CompletedTask;
+    }
+}
