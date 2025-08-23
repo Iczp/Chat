89bf22ccdb7a6501775aa30008d64da742a84d1a
zhongpei <1000@intry.cn>
2025-08-23 17:49:45 +0800

    OK

--- src/IczpNet.Chat.Domain/Follows/Follow.cs
+++ src/IczpNet.Chat.Domain/Follows/Follow.cs
@@ -1,5 +1,6 @@
 ﻿using IczpNet.Chat.BaseEntities;
 using IczpNet.Chat.ChatObjects;
+using IczpNet.Chat.Enums;
 using IczpNet.Chat.SessionUnits;
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,17 +12,37 @@ namespace IczpNet.Chat.Follows;
 /// </summary>
 public class Follow : BaseEntity
 {
+    /// <summary>
+    /// ChatObjectId
+    /// </summary>
+    public virtual long? OwnerId { get; protected set; }
 
-    ///// <summary>
-    ///// ChatObjectId
-    ///// </summary>
-    //public virtual int? OwnerId { get; protected set; }
+    /// <summary>
+    /// ChatObject
+    /// </summary>
+    [ForeignKey(nameof(OwnerId))]
+    public virtual ChatObject Owner { get; protected set; }
 
-    ///// <summary>
-    ///// ChatObject
-    ///// </summary>
-    //[ForeignKey(nameof(OwnerId))]
-    //public virtual ChatObject Owner { get; protected set; }
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual ChatObjectTypeEnums? OwnerType { get; protected set; }
+
+    /// <summary>
+    /// ChatObjectId
+    /// </summary>
+    public virtual long? DestinationId { get; protected set; }
+
+    /// <summary>
+    /// Target ChatObject
+    /// </summary>
+    [ForeignKey(nameof(DestinationId))]
+    public virtual ChatObject Destination { get; protected set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual ChatObjectTypeEnums? DestinationType { get; protected set; }
 
     /// <summary>
     /// Owner SessionUnitId
@@ -39,8 +60,8 @@ public class Follow : BaseEntity
     /// </summary>
     public virtual Guid DestinationSessionUnitId { get; protected set; }
 
-    //[ForeignKey(nameof(DestinationId))]
-    //public virtual SessionUnit Destination { get; set; }
+    //[ForeignKey(nameof(OwnerSessionUnitId))]
+    //public virtual SessionUnit DestinationSessionUnit { get; set; }
 
     public override object[] GetKeys()
     {
