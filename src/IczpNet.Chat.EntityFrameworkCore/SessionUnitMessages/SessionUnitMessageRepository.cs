acd71722eeac2628518c08399980dc932be55c79
zhongpei <1000@intry.cn>
2025-11-21 11:30:07 +0800

    IsRemindMe

--- src/IczpNet.Chat.EntityFrameworkCore/SessionUnitMessages/SessionUnitMessageRepository.cs
+++ src/IczpNet.Chat.EntityFrameworkCore/SessionUnitMessages/SessionUnitMessageRepository.cs
@@ -1,5 +1,6 @@
 ﻿using IczpNet.Chat.EntityFrameworkCore;
 using IczpNet.Chat.Follows;
+using IczpNet.Chat.MessageSections.MessageReminders;
 using IczpNet.Chat.MessageSections.Messages;
 using IczpNet.Chat.Repositories;
 using IczpNet.Chat.SessionUnits;
@@ -19,6 +20,7 @@ public class SessionUnitMessageRepository(IDbContextProvider<ChatDbContext> dbCo
         var sessionUnitMessageTableName = $"{ChatDbProperties.DbTablePrefix}_{nameof(SessionUnitMessage)}";
         var followTableName = $"{ChatDbProperties.DbTablePrefix}_{nameof(Follow)}";
         var sessionUnitTableName = $"{ChatDbProperties.DbTablePrefix}_{nameof(SessionUnit)}";
+        var messageReminderTableName = $"{ChatDbProperties.DbTablePrefix}_{nameof(MessageReminder)}";
         var sql = $@"
 INSERT INTO [{sessionUnitMessageTableName}] (
     [SessionUnitId],
@@ -40,6 +42,7 @@ SELECT
     0 AS IsRead,
     0 AS IsOpened,
     0 AS IsFavorited,
+
     -- IsFollowing 动态查询
     CASE 
         WHEN EXISTS (
@@ -49,7 +52,17 @@ SELECT
               AND f.DestinationSessionUnitId = @SenderSessionUnitId
         ) THEN 1 ELSE 0 
     END AS IsFollowing,
-    0 AS IsRemindMe,
+
+    -- IsRemindMe
+    CASE 
+        WHEN EXISTS (
+            SELECT 1 
+            FROM [{messageReminderTableName}] r
+            WHERE r.[SessionUnitId] = su.Id
+              AND r.[MessageId] = @MessageId
+        ) THEN 1 ELSE 0 
+    END AS IsRemindMe,
+
     0 AS IsDeleted,
     @ExtraProperties AS ExtraProperties,
     REPLACE(NEWID(), '-', '') AS ConcurrencyStamp,
