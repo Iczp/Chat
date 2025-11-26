35097057f8e9f17c6a22cf9fa07aac5fe5e491dc
zhongpei <1000@intry.cn>
2025-11-26 18:13:56 +0800

    CreateMap<SessionUnitCacheDto, SessionUnitCacheDto>()
                .ForMember(x => x.Settings, opt => opt.MapFrom(y => y.Settings))
                .ForMember(x => x.Destination, opt => opt.MapFrom(y => y.Destination))
                .PreserveReferences()
                .MaxDepth(3);

--- src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitCacheDto.cs
+++ src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitCacheDto.cs
@@ -1,15 +1,11 @@
 ﻿using IczpNet.Chat.ChatObjects;
 using IczpNet.Chat.SessionSections.SessionUnits;
 using IczpNet.Chat.SessionUnitSettings;
-using System;
 
 namespace IczpNet.Chat.SessionUnits.Dtos;
 
-public class SessionUnitCacheDto
+public class SessionUnitCacheDto: SessionUnitCacheItem
 {
-    public Guid Id { get; set; }
-
-    public virtual SessionUnitCacheItem Item { get; set; }
 
     public virtual SessionUnitSettingCacheItem Settings { get; set; }
 
--- src/IczpNet.Chat.Application/SessionUnits/SessionUnitCacheAppService.cs
+++ src/IczpNet.Chat.Application/SessionUnits/SessionUnitCacheAppService.cs
@@ -1,8 +1,11 @@
-﻿using IczpNet.Chat.BaseAppServices;
+﻿using IczpNet.AbpCommons.DataFilters;
+using IczpNet.Chat.BaseAppServices;
 using IczpNet.Chat.Follows;
 using IczpNet.Chat.Permissions;
+using IczpNet.Chat.SessionSections.SessionUnits;
 using IczpNet.Chat.SessionUnits.Dtos;
 using IczpNet.Chat.SessionUnitSettings;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
@@ -34,62 +37,68 @@ public class SessionUnitCacheAppService(
     {
 
         //var followingList = await FollowManager.GetFollowingIdListAsync(input.OwnerId);
-        var list = await SessionUnitCacheManager.GetOrSetListByOwnerAsync(
+        var cacheList = await SessionUnitCacheManager.GetOrSetListByOwnerAsync(
             input.OwnerId,
             async (ownerId) =>
                 await SessionUnitManager.GetListByOwnerIdAsync(ownerId));
 
-        var query = list.Select(x => new SessionUnitCacheDto()
-        {
-            Id = x.Id,
-            Item = x,
-        }).AsQueryable()
-            .WhereIf(input.DestinationId.HasValue, x => x.Item.DestinationId == input.DestinationId)
-            .WhereIf(input.DestinationObjectType.HasValue, x => x.Item.DestinationObjectType == input.DestinationObjectType)
-            //.WhereIf(input.IsKilled.HasValue, x => x.Item.Setting.IsKilled == input.IsKilled)
-            //.WhereIf(input.IsCreator.HasValue, x => x.Item.Setting.IsCreator == input.IsCreator)
-            .WhereIf(input.MinMessageId.HasValue, x => x.Item.LastMessageId >= input.MinMessageId)
-            .WhereIf(input.MaxMessageId.HasValue, x => x.Item.LastMessageId < input.MaxMessageId)
-            .WhereIf(input.MinTicks.HasValue, x => x.Item.Ticks >= input.MinTicks)
-            .WhereIf(input.MaxTicks.HasValue, x => x.Item.Ticks < input.MaxTicks)
-            //.WhereIf(input.IsTopping == true, x => x.Item.Sorting > 0)
-            //.WhereIf(input.IsTopping == false, x => x.Item.Sorting == 0)
-            //.WhereIf(input.IsContacts.HasValue, x => x.Item.Setting.IsContacts == input.IsContacts)
-            //.WhereIf(input.IsImmersed.HasValue, x => x.Item.Setting.IsImmersed == input.IsImmersed)
-            .WhereIf(input.IsBadge.HasValue, x => x.Item.PublicBadge > 0)
+        var list = ObjectMapper.Map<List<SessionUnitCacheItem>, List<SessionUnitCacheDto>>(cacheList.ToList());
 
-            //@我
-            .WhereIf(input.IsRemind == true, x => (x.Item.RemindAllCount + x.Item.RemindMeCount) > 0)
-            .WhereIf(input.IsRemind == false, x => (x.Item.RemindAllCount + x.Item.RemindMeCount) == 0)
-
-
-            //我关注的
-            //.WhereIf(input.IsFollowing.HasValue, x => x.Item.FollowingCount > 0)
-            //.WhereIf(input.IsFollowing == true, x => x.Item.FollowingList.Count > 0)
-            //.WhereIf(input.IsFollowing == false, x => x.Item.FollowingList.Count == 0)
-
-            //关注我的
-            //.WhereIf(input.IsFollower == true, x => x.Item.FollowerList.Count > 0)
-            //.WhereIf(input.IsFollower == false, x => x.Item.FollowerList.Count == 0)
-            ;
-
-        var pagedList = await GetPagedListAsync(query, input);
-
-        var unitIds = pagedList.Items.Select(x => x.Id).ToList();
+        var unitIds = list.Select(x => x.Id).ToList();
 
         var settingMap = await SessionUnitSettingManager.GetCacheManyAsync(unitIds);
 
-        var destIdList = pagedList.Items.Where(x => x.Item.DestinationId.HasValue).Select(x => x.Item.DestinationId.Value).Distinct().ToList();
+        var destIdList = list.Where(x => x.DestinationId.HasValue).Select(x => x.DestinationId.Value).Distinct().ToList();
 
         var destList = await ChatObjectManager.GetManyByCacheAsync(destIdList);
 
         var destMap = destList.ToDictionary(x => x.Id, x => x);
 
-        foreach (var item in pagedList.Items)
+        foreach (var item in list)
         {
+            item.Destination = item.DestinationId.HasValue ? destMap[item.DestinationId.Value] : null;
             item.Settings = settingMap[item.Id] ?? null;
-            item.Destination = item.Item.DestinationId.HasValue ? destMap[item.Item.DestinationId.Value] ?? null : null;
         }
+
+        var query = list.AsQueryable()
+            .WhereIf(input.DestinationId.HasValue, x => x.DestinationId == input.DestinationId)
+            .WhereIf(input.DestinationObjectType.HasValue, x => x.DestinationObjectType == input.DestinationObjectType)
+            //.WhereIf(input.IsKilled.HasValue, x => x.Setting.IsKilled == input.IsKilled)
+            //.WhereIf(input.IsCreator.HasValue, x => x.Setting.IsCreator == input.IsCreator)
+            .WhereIf(input.MinMessageId.HasValue, x => x.LastMessageId >= input.MinMessageId)
+            .WhereIf(input.MaxMessageId.HasValue, x => x.LastMessageId < input.MaxMessageId)
+            .WhereIf(input.MinTicks.HasValue, x => x.Ticks >= input.MinTicks)
+            .WhereIf(input.MaxTicks.HasValue, x => x.Ticks < input.MaxTicks)
+            //.WhereIf(input.IsTopping == true, x => x.Sorting > 0)
+            //.WhereIf(input.IsTopping == false, x => x.Sorting == 0)
+            //.WhereIf(input.IsContacts.HasValue, x => x.Setting.IsContacts == input.IsContacts)
+            //.WhereIf(input.IsImmersed.HasValue, x => x.Setting.IsImmersed == input.IsImmersed)
+            .WhereIf(input.IsBadge.HasValue, x => x.PublicBadge > 0)
+
+            //@我
+            .WhereIf(input.IsRemind == true, x => (x.RemindAllCount + x.RemindMeCount) > 0)
+            .WhereIf(input.IsRemind == false, x => (x.RemindAllCount + x.RemindMeCount) == 0)
+
+
+            //我关注的
+            //.WhereIf(input.IsFollowing.HasValue, x => x.FollowingCount > 0)
+            //.WhereIf(input.IsFollowing == true, x => x.FollowingList.Count > 0)
+            //.WhereIf(input.IsFollowing == false, x => x.FollowingList.Count == 0)
+
+            //关注我的
+            //.WhereIf(input.IsFollower == true, x => x.FollowerList.Count > 0)
+            //.WhereIf(input.IsFollower == false, x => x.FollowerList.Count == 0)
+            ;
+
+        var pagedList = await GetPagedListAsync(query, input);
+
+
+
+        //foreach (var item in pagedList.Items)
+        //{
+        //    item.Settings = settingMap[item.Id] ?? null;
+        //    item.Destination = item.Item.DestinationId.HasValue ? destMap[item.Item.DestinationId.Value] ?? null : null;
+        //}
 
         return pagedList;
 
