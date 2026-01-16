00dc60278c829cbc1c5004e5b4567fa72cfd3fc2
zhongpei <1000@intry.cn>
2026-01-16 18:42:00 +0800

    DestinationType

--- src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitFirendGetListInput.cs
+++ src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitFirendGetListInput.cs
@@ -4,10 +4,7 @@ namespace IczpNet.Chat.SessionUnits.Dtos;
 
 public class SessionUnitFirendGetListInput : SessionUnitLatestGetListInput
 {
-    /// <summary>
-    /// 是否在线
-    /// </summary>
-    public bool? IsOnline { get; set; }
+
 
     /// <summary>
     /// 好友归档
@@ -21,6 +18,26 @@ public class SessionUnitFirendGetListInput : SessionUnitLatestGetListInput
     /// - 7=HasBadge:有未读消息
     /// </summary>
     public FriendTypes FriendType { get; set; }
+
+    /// <summary>
+    /// 是否在线
+    /// </summary>
+    public bool? IsOnline { get; set; }
+
+    /// <summary>
+    /// 聊天对象类型:个人|群|服务号等
+    /// - 0=Anonymous:匿名
+    /// - 1=Personal:个人
+    /// - 2=Room:群
+    /// - 3=Official:服务号
+    /// - 4=Subscription:订阅号
+    /// - 5=Square:广场
+    /// - 6=Robot:机器人
+    /// - 7=ShopKeeper:掌柜
+    /// - 8=ShopWaiter:店小二
+    /// - 9=Customer:客户
+    /// </summary>
+    public ChatObjectTypeEnums? DestinationType { get; set; }
 
 
 }
