c67a895cd1033db10dee2cb7cc439b0657763077
zhongpei <1000@intry.cn>
2026-03-10 14:05:35 +0800

    0

--- src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitMemberDto.cs
+++ src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitMemberDto.cs
@@ -1,40 +1,10 @@
-﻿using IczpNet.Chat.ChatObjects;
-using IczpNet.Chat.Enums;
-using IczpNet.Chat.SessionTags;
-using IczpNet.Chat.SessionUnitSettings.Dtos;
+﻿using IczpNet.Chat.Enums;
 using System;
-using System.Collections.Generic;
 
 namespace IczpNet.Chat.SessionUnits.Dtos;
 
-public class SessionUnitMemberDto 
+public class SessionUnitMemberDto : SessionUnitMemberSenderDto
 {
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public virtual Guid Id { get; set; }
-
-    /// <summary>
-    /// 会话Id
-    /// </summary>
-    public virtual Guid? SessionId { get; set; }
-
-    /// <summary>
-    /// OwnerId
-    /// </summary>
-    public virtual long OwnerId { get; set; }
-
-    /// <summary>
-    /// OwnerType
-    /// </summary>
-    public virtual ChatObjectTypeEnums? OwnerObjectType { get; set; }
-
-    /// <summary>
-    /// 会话内名称
-    /// </summary>
-    public virtual string MemberName { get; set; }
-
     /// <summary>
     /// DestinationId
     /// </summary>
@@ -44,6 +14,7 @@ public class SessionUnitMemberDto
     /// DestinationType
     /// </summary>
     public virtual ChatObjectTypeEnums? DestinationObjectType { get; set; }
+
 
     /// <summary>
     /// Ticks
@@ -59,25 +30,5 @@ public class SessionUnitMemberDto
     /// 
     /// </summary>
     public virtual DateTime CreationTime { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public virtual ChatObjectInfo Owner { get; set; }
-
-    /// <summary>
-    /// 好友关系
-    /// </summary>
-    public virtual SessionUnitFriendshipDto Friendship { get; set; }
-
-    /// <summary>
-    /// 设置
-    /// </summary>
-    public virtual SessionUnitMemberSettingDto Setting { get; set; }
-
-    /// <summary>
-    /// 标签
-    /// </summary>
-    public virtual List<SessionTagCacheItem> TagList { get; set; }
 
 }
---
+++ src/IczpNet.Chat.Application.Contracts/SessionUnits/Dtos/SessionUnitMemberSenderDto.cs
@@ -0,0 +1,57 @@
+﻿using IczpNet.Chat.ChatObjects;
+using IczpNet.Chat.Enums;
+using IczpNet.Chat.SessionTags;
+using IczpNet.Chat.SessionUnitSettings.Dtos;
+using System;
+using System.Collections.Generic;
+
+namespace IczpNet.Chat.SessionUnits.Dtos;
+
+public class SessionUnitMemberSenderDto
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual Guid Id { get; set; }
+
+    /// <summary>
+    /// 会话Id
+    /// </summary>
+    public virtual Guid? SessionId { get; set; }
+
+    /// <summary>
+    /// OwnerId
+    /// </summary>
+    public virtual long OwnerId { get; set; }
+
+    /// <summary>
+    /// OwnerType
+    /// </summary>
+    public virtual ChatObjectTypeEnums? OwnerObjectType { get; set; }
+
+    /// <summary>
+    /// 会话内名称
+    /// </summary>
+    public virtual string MemberName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public virtual ChatObjectInfo Owner { get; set; }
+
+    /// <summary>
+    /// 好友关系
+    /// </summary>
+    public virtual SessionUnitFriendshipDto Friendship { get; set; }
+
+    /// <summary>
+    /// 设置
+    /// </summary>
+    public virtual SessionUnitMemberSettingDto Setting { get; set; }
+
+    /// <summary>
+    /// 标签
+    /// </summary>
+    public virtual List<SessionTagCacheItem> TagList { get; set; }
+}
